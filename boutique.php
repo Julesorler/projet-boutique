ef12441e80a3e07cadfeebb1a3a1b09be81c2f06
Julesorler <jules.orler89@outlook.fr>
2024-11-06 11:42:59 +0100

    boutique.php

--- boutique.php
+++ boutique.php
@@ -1,6 +1,6 @@
 <?php
 
-$articles = ["debardeur", "Torchon", "sandale", "bonnet", "veste"];
+$produit= ["debardeur", "Torchon", "sandale", "bonnet", "veste"];
 $quantites = [102, 175, 68, 43, 212];
 $ventes = [0, 0, 0, 0, 0];
 
@@ -13,11 +13,11 @@ while ($choix != 6) {
 
 
     echo "\naccueil:\n";
-    echo "1. articles disponibles et quantités\n";
+    echo "1. produits disponibles et quantités\n";
     echo "2. vente\n";
-    echo "3. Réapprovisionner un article\n";
+    echo "3. Réapprovisionner un produit\n";
     echo "4. stock\n";
-    echo "5. ventes totales par article\n";
+    echo "5. ventes totales par produit\n";
     echo "6. Supprimer \n";
 
 
@@ -32,9 +32,9 @@ while ($choix != 6) {
 
     if ($choix == 1) {
         echo "\nArticles disponibles avec leurs quantités :\n";
-        for ($i = 0; $i < count($articles); $i++) {
+        for ($i = 0; $i < count($produit); $i++) {
             if ($quantites[$i] > 0) {
-                echo "$i: $articles[$i] - Quantité : $quantites[$i]\n";
+                echo "$i: $produit[$i] - Quantité : $quantites[$i]\n";
             }
         }
     }
@@ -46,20 +46,20 @@ while ($choix != 6) {
 
 
         echo "\nArticles disponibles avec leurs quantités :\n";
-        for ($i = 0; $i < count($articles); $i++) {
-            echo "$i: $articles[$i] - Quantité : $quantites[$i]\n";
+        for ($i = 0; $i < count($produit); $i++) {
+            echo "$i: $produit[$i] - Quantité : $quantites[$i]\n";
         }
 
         $index = (readline("Choisissez l'article à vendre : "));
         $quantiteVendue = (readline("Quantité vendue : "));
 
-        if ($index >= 0 && $index < count($articles)) {
+        if ($index >= 0 && $index < count($produit)) {
             if ($quantites[$index] >= $quantiteVendue) {
                 $quantites[$index] -= $quantiteVendue;
                 $ventes[$index] += $quantiteVendue;
-                echo "DONE: $quantiteVendue $articles[$index]\n";
+                echo "DONE: $quantiteVendue $produit[$index]\n";
             } else {
-                echo "Stock insuffisant $articles[$index].\n";
+                echo "Stock insuffisant $produit[$index].\n";
             }
         } else {
             echo "erreur\n";
@@ -73,8 +73,8 @@ while ($choix != 6) {
 
 
 
-        for ($i = 0; $i < count($articles); $i++) {
-            echo "$i: $articles[$i] - Quantité : $quantites[$i]\n";
+        for ($i = 0; $i < count($produit); $i++) {
+            echo "$i: $produit[$i] - Quantité : $quantites[$i]\n";
         }
 
 
@@ -82,7 +82,7 @@ while ($choix != 6) {
 
         $quantiteReapro = (readline("Quantité à réapprovisionner : "));
         $quantites[$index] += $quantiteReapro;
-        echo "DONE : + $quantiteReapro $articles[$index]\n";
+        echo "DONE : + $quantiteReapro $produit[$index]\n";
     }
 
 
@@ -90,13 +90,13 @@ while ($choix != 6) {
     if ($choix == 4) {
         echo "\n stock :\n";
 
-        for ($i = 0; $i < count($articles); $i++) {
+        for ($i = 0; $i < count($produit); $i++) {
 
-            echo "$articles[$i] - Quantité restante : $quantites[$i] \n";
+            echo "$produit[$i] - Quantité restante : $quantites[$i] \n";
 
 
             if ($quantites[$i] == 0) {
-                echo " $articles[$i] est en rupture de stock !\n";
+                echo " $produit[$i] est en rupture de stock !\n";
                 $tousEnStock = false;
             }
         }
@@ -107,9 +107,9 @@ while ($choix != 6) {
     if ($choix == 5) {
         echo "\n ventes totales par article :\n";
 
-        for ($i = 0; $i < count($articles); $i++) {
+        for ($i = 0; $i < count($produit); $i++) {
 
-            echo "$articles[$i] - Quantité vendue : $ventes[$i] \n";
+            echo "$produit[$i] - Quantité vendue : $ventes[$i] \n";
         }
     }
 
@@ -118,17 +118,17 @@ while ($choix != 6) {
         echo "Que voulez-vous supprimer ? : \n";
 
 
-        for ($i = 0; $i < count($articles); $i++) {
-            echo "$i: $articles[$i] - Quantité : $quantites[$i]\n";
+        for ($i = 0; $i < count($produit); $i++) {
+            echo "$i: $produit[$i] - Quantité : $quantites[$i]\n";
         }
 
 
         $index = (readline("Choisissez l'article à supprimer : "));
-        echo "Article supprimé : $articles[$index]\n";
+        echo "Produit supprimé : $produit[$index]\n";
 
-        if ($index >= 0 && $index < count($articles)) {
+        if ($index >= 0 && $index < count($produit)) {
 
-            array_splice($articles, $index, 1);
+            array_splice($produit, $index, 1);
             array_splice($quantites, $index, 1);
             array_splice($ventes, $index, 1);
         } else {
